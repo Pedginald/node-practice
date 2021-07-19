3f38bea01e1a2db929b3737efcf5ddc8e056873f
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-07-19 15:41:55 -0400

    Adding example of setTimeout method

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,7 @@
+// Started operating system process
+console.log('First');
+setTimeout(() => {
+	console.log('second');
+}, 0)
+console.log('third');
+// Completed and excited operating system process
