61d371a0953666014a12fbb40734411475cc7c40
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-07-20 15:22:09 -0400

    Adding comments to event emitter code

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -1,6 +1,14 @@
+// Get back the class
+// if you want custom extend from class
+// otherwise just for emitting and handling events create instance
 const eventEmitter = require('events');
 
 const customEmitter = new eventEmitter();
+
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
 
 customEmitter.on('response', (name, id) => {
 	console.log(`Data received user ${name} with id: ${id}`);
